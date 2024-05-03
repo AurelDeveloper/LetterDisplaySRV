83ed211eacd7604ed31c8bba2a4b4a5f3145c6bd
AurelDeveloper <marlon.profils@gmail.com>
2024-05-03 13:03:27 +0200

    .

--- src/utils/Article.py
+++ src/utils/Article.py
@@ -1,8 +1,9 @@
 class Article:
-    def __init__(self, title, image, date, content, snippet, readtime):
+    def __init__(self, title, image, date, content, snippet, readtime, tag):
         self.title = title
         self.image = image
         self.date = date
         self.content = content
         self.snippet = snippet
         self.readtime = readtime
+        self.tag = tag
--- src/utils/extractor.py
+++ src/utils/extractor.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import datetime
+import os
 from Article import Article
 from supabase_client import supabase
 
@@ -14,6 +15,14 @@ def calculate_readtime(email_content):
     readtime = num_words / 250
     return round(readtime)
 
+def get_most_common_tag(content):
+    tags = os.getenv('TAGS').split(',')
+    tag_counts = {tag: content.lower().count(tag) for tag in tags}
+    most_common_tag = max(tag_counts, key=tag_counts.get)
+    if tag_counts[most_common_tag] == 0:
+        return os.getenv('DEFAULT_TAG')
+    return most_common_tag
+
 def extract(email_content):
     soup = BeautifulSoup(email_content, 'html.parser')
 
@@ -23,8 +32,9 @@ def extract(email_content):
     content = str(soup)
     snippet = soup.find('p').text[:40] if soup.find('p') else "No snippet found"
     readtime = calculate_readtime(content)
+    tag = get_most_common_tag(content)
 
-    article = Article(title, image, date, content, snippet, readtime)
+    article = Article(title, image, date, content, snippet, readtime, tag)
 
     return article
 
@@ -35,7 +45,8 @@ def upload(article):
         "date": article.date,
         "content": article.content,
         "snippet": article.snippet,
-        "readtime": article.readtime
+        "readtime": article.readtime,
+        "tag": article.tag
     }).execute()
 
 if __name__ == "__main__":
